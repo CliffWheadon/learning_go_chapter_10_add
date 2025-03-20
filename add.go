171a6c859ac29f7fe290f27a7969f464672de5ba
cliff.wheadon <cliff.wheadon@nielsen.com>
2025-03-20 16:53:39 -0300

    Adding GoDoc Comments

--- add.go
+++ add.go
@@ -1,5 +1,13 @@
+// Package learning_go_chapter_10_add contains an Add method that meets the specification of the Exercise in chapter 10 of Learning Go
 package learning_go_chapter_10_add
 
+// Add adds two ints together
+//
+// It has two parameters: a and b which are both integers. It returns an integer representing the sum of a and b
+//
+// For more information on addition, see: [Math is Fun]
+//
+// [Math is Fun]: https://www.mathsisfun.com/numbers/addition.html
 func Add(a, b int) int {
 	return a + b
 }
